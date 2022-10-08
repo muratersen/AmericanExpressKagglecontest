e179250b1b504572e08c6e82dd5d42272081ea45
mersen <102302684+muratersen@users.noreply.github.com>
2022-10-08 14:36:56 +0300

    Create trainin.py

---
+++ trainin.py
@@ -0,0 +1,142 @@
+from cgi import test
+import os
+import gc
+import warnings
+warnings.filterwarnings('ignore')
+import random
+import scipy as sp
+import numpy as np
+import pandas as pd
+import joblib
+import itertools
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+from tqdm.auto import tqdm
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.preprocessing import LabelEncoder
+import lightgbm as lgb
+from itertools import combinations
+
+class CFG:
+    input_dir = '/Users/muratersenunal/Desktop/pythonprojects/AmericanExpress/'
+    seed = 42
+    n_folds = 5
+    target = 'target'
+def seed_everything(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+def read_data():
+    train = pd.read_parquet(CFG.input_dir + 'traina.parquet')
+    test = pd.read_parquet(CFG.input_dir + 'testa.parquet')
+    print(test.shape)
+    return train, test
+def amex_metric(y_true, y_pred):
+    labels = np.transpose(np.array([y_true, y_pred]))
+    labels = labels[labels[:, 1].argsort()[::-1]]
+    weights = np.where(labels[:,0]==0, 20, 1)
+    cut_vals = labels[np.cumsum(weights) <= int(0.04 * np.sum(weights))]
+    top_four = np.sum(cut_vals[:,0]) / np.sum(labels[:,0])
+    gini = [0,0]
+    for i in [1,0]:
+        labels = np.transpose(np.array([y_true, y_pred]))
+        labels = labels[labels[:, i].argsort()[::-1]]
+        weight = np.where(labels[:,0]==0, 20, 1)
+        weight_random = np.cumsum(weight / np.sum(weight))
+        total_pos = np.sum(labels[:, 0] *  weight)
+        cum_pos_found = np.cumsum(labels[:, 0] * weight)
+        lorentz = cum_pos_found / total_pos
+        gini[i] = np.sum((lorentz - weight_random) * weight)
+    return 0.5 * (gini[1]/gini[0] + top_four)
+def lgb_amex_metric(y_pred, y_true):
+    y_true = y_true.get_label()
+    return 'amex_metric', amex_metric(y_true, y_pred), True
+def train_and_evaluate(train, test):
+    # Label encode categorical features
+    cat_features = [
+        "B_30",
+        "B_38",
+        "D_114",
+        "D_116",
+        "D_117",
+        "D_120",
+        "D_126",
+        "D_63",
+        "D_64",
+        "D_66",
+        "D_68"
+    ]
+    cat_features = [f"{cf}_last" for cf in cat_features]
+    for cat_col in cat_features:
+        encoder = LabelEncoder()
+        train[cat_col] = encoder.fit_transform(train[cat_col])
+        test[cat_col] = encoder.transform(test[cat_col])
+    # Round last float features to 2 decimal place
+    num_cols = list(train.dtypes[(train.dtypes == 'float32') | (train.dtypes == 'float64')].index)
+    num_cols = [col for col in num_cols if 'last' in col]
+    for col in num_cols:
+        train[col + '_round2'] = train[col].round(2)
+        test[col + '_round2'] = test[col].round(2)
+    # Get feature list
+    features = [col for col in train.columns if col not in ['customer_ID', CFG.target]]
+    params = {
+        'objective': 'binary',
+        'metric': "binary_logloss",
+        'boosting': 'dart',
+        'seed': CFG.seed,
+        'num_leaves': 100,
+        'learning_rate': 0.01,
+        'feature_fraction': 0.20,
+        'bagging_freq': 10,
+        'bagging_fraction': 0.50,
+        'n_jobs': -1,
+        'lambda_l2': 2,
+        'min_data_in_leaf': 40
+        }
+    # Create a numpy array to store test predictions
+    test_predictions = np.zeros(len(test))
+    # Create a numpy array to store out of folds predictions
+    oof_predictions = np.zeros(len(train))
+    kfold = StratifiedKFold(n_splits = CFG.n_folds, shuffle = True, random_state = CFG.seed)
+    for fold, (trn_ind, val_ind) in enumerate(kfold.split(train, train[CFG.target])):
+        print(' ')
+        print('-'*50)
+        print(f'Training fold {fold} with {len(features)} features...')
+        x_train, x_val = train[features].iloc[trn_ind], train[features].iloc[val_ind]
+        y_train, y_val = train[CFG.target].iloc[trn_ind], train[CFG.target].iloc[val_ind]
+        lgb_train = lgb.Dataset(x_train, y_train, categorical_feature = cat_features)
+        lgb_valid = lgb.Dataset(x_val, y_val, categorical_feature = cat_features)
+        model = lgb.train(
+            params = params,
+            train_set = lgb_train,
+            num_boost_round = 10500,
+            valid_sets = [lgb_train, lgb_valid],
+            early_stopping_rounds = 3,
+            verbose_eval = 500,
+            feval = lgb_amex_metric
+            )
+        # Save best model
+        # Predict validation
+        val_pred = model.predict(x_val)
+        # Add to out of folds array
+        oof_predictions[val_ind] = val_pred
+        # Predict the test set
+        test_pred = model.predict(test[features])
+        test_predictions += test_pred / CFG.n_folds
+        # Compute fold metric
+        score = amex_metric(y_val, val_pred)
+        print(f'Our fold {fold} CV score is {score}')
+        del x_train, x_val, y_train, y_val, lgb_train, lgb_valid
+        gc.collect()
+    # Compute out of folds metric
+    score = amex_metric(train[CFG.target], oof_predictions)
+    print(f'Our out of folds CV score is {score}')
+    # Create a dataframe to store out of folds predictions
+    return test_predictions
+submission = pd.read_csv(CFG.input_dir + 'sample_submission 2.csv')
+seed_everything(CFG.seed)
+train, test = read_data()
+array=train_and_evaluate(train, test)
+submission["prediction"]=array
+submission.to_csv(CFG.input_dir + 'sample_submission.csv')
